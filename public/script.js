adff2d2c1bcd4468212cdddfb15d1849bead5d52
Glitch (hello-express) <none>
2020-05-18 21:10:28 +0000

    ☂️🐆 Checkpoint ./public/script.js:16004622/449 ./.glitch-assets:16004622/739

--- public/script.js
+++ public/script.js
@@ -2,4 +2,10 @@ var socket = io.connect("https://tac-tic-toe.glitch.me/");
 
 const squares = document.querySelectorAll(".emptysquare");
 
+function selectSquare(){
+  this.removeEventListener('click', selectSquare);
+  this.setAttribute('src', 'https://cdn.glitch.com/1613f632-e960-433e-9ef8-9778ae67ee09%2Ftoe.jpg?v=1589836195603');
+}
+
+squares.forEach(square => square.addEventListener('click', selectSquare));
 
