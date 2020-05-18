9ea7aee96b0db71912e5381e08ef3f76a5f48114
Glitch (hello-express) <none>
2020-05-18 21:21:43 +0000

    💋🌦 Checkpoint ./public/script.js:16004622/782 ./.glitch-assets:16004622/417

--- public/script.js
+++ public/script.js
@@ -1,11 +1,16 @@
 var socket = io.connect("https://tac-tic-toe.glitch.me/");
 
 const squares = document.querySelectorAll(".emptysquare");
+const handle = document.getElementById("handle");
 
 function selectSquare(){
+  socket.emit("move", handle.value);
   this.removeEventListener('click', selectSquare);
-  this.setAttribute('src', 'https://cdn.glitch.com/1613f632-e960-433e-9ef8-9778ae67ee09%2Ftoe.jpg?v=1589836195603');
+  this.setAttribute('src', 'https://cdn.glitch.com/1613f632-e960-433e-9ef8-9778ae67ee09%2Ftictac.jpg?v=1589836612833');
 }
 
-squares.forEach(square => square.addEventListener('click', selectSquare));
+squares.forEach(square => square.addEventListener("keypress", function(){
+  socket.emit("typing", handle.value);
+  })
+);
 
